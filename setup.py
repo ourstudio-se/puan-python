8ebead91f608e845cf0151712e697b23dbd44a52
Rikard Olsson <rikard@thryselius.se>
2022-10-05 11:21:19 +0200

    Using version of puan-rspy which is compatible with ubuntu

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
             "more-itertools",
             "maz>=0.0.1",
             "dictdiffer==0.9.0",
-            "puan-rspy==0.1.0",
+            "puan-rspy>=0.1.8",
             "toposort==1.7"
         ],
         packages=[
