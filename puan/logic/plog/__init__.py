bb3adc838c39eaab5544c2b502ffdee9623dfada
ourmoa <100134345+ourmoa@users.noreply.github.com>
2022-12-27 10:50:48 +0100

    Update puan/logic/plog/__init__.py
    Co-authored-by: ourmarina <121033780+ourmarina@users.noreply.github.com>

--- puan/logic/plog/__init__.py
+++ puan/logic/plog/__init__.py
@@ -1588,7 +1588,7 @@ class Equal(All):
     
     def __init__(self, value: int, propositions: typing.List[typing.Union[str, puan.variable]], variable: typing.Union[str, puan.variable] = None):
         super().__init__(
-            AtLeast(value=value, propositions=propositions, variable=variable),
+            AtLeast(value=value, propositions=propositions),
             AtMost(value=value, propositions=propositions, variable=variable))
     
 
